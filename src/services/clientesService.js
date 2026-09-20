a082eeeaf8ce78a92ecf044a142619d33481e5cd
Quispe Ortiz Agustina Paola <agustinaquispe30102006@gmail.com>
2026-09-20 14:55:21 -0400

    refactor: centraliza operaciones HTTP en clientesService (H011)

--- src/services/clientesService.js
+++ src/services/clientesService.js
@@ -2,16 +2,29 @@ import axios from "axios";
 
 const URL = "https://fakestoreapi.com/users";
 
+const obtenerClientes = async () => {
+    const respuesta = await axios.get(URL);
+    return respuesta.data;
+};
+
+const obtenerCliente = async (id) => {
+    const respuesta = await axios.get(`${URL}/${id}`);
+    return respuesta.data;
+};
+
 const crearCliente = async (cliente) => {
+    const respuesta = await axios.post(URL, cliente);
+    return respuesta.data;
+};
 
-    const respuesta = await axios.post(
-        URL,
-        cliente
-    );
-
+const eliminarCliente = async (id) => {
+    const respuesta = await axios.delete(`${URL}/${id}`);
     return respuesta.data;
 };
 
 export default {
-    crearCliente
+    obtenerClientes,
+    obtenerCliente,
+    crearCliente,
+    eliminarCliente
 };
